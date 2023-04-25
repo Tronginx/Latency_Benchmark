7f8d42569845945c54c622938b22f6de8c4fe7d5
troantian2022 <trontian2022@gmail.com>
2023-04-24 23:34:38 -0400

    test from 1 to 16000 bytes

--- run_sender.sh
+++ run_sender.sh
@@ -3,7 +3,7 @@ output_file="sender_output.csv"
 
 echo "Message Size (bytes),Average Latency (ms)" > $output_file
 
-for i in {1..1600}; do
+for i in {1..16000}; do
     echo "Running test for message size: $i bytes"
     output=$(./latency_benchmark sender $i)
     echo "$i,$output" >> $output_file
@@ -12,3 +12,13 @@ done
 
 echo "All tests completed."
 
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"run_sender.sh" 14L, 355C                                     14,0-1        All
